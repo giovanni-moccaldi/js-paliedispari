4ef5683ac1c84ffddb65cfb05587c25b64534213
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-01-31 17:01:03 +0100

    pari e dispari

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -3,13 +3,18 @@ let number = parseInt(prompt("scegli un numero da 1 a 5"));
 console.log(number);
 
 randomnumber();
+somma();
 
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 function randomnumber() {
-    const mypc = Math.floor(Math.random() * 5) + 1;
+    let mypc = parseInt(Math.floor(Math.random() * 5)) + 1;
     console.log(mypc);
 
 }
-// Sommiamo i due numeri
+// // Sommiamo i due numeri
+function somma() {
+    let addizzione = number + mypc;
+    console.log(addizzione);
+}
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
