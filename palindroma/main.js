6d47be6e8491184410193c64a4719741d6dd8720
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-01-31 17:44:41 +0100

    done

--- palindroma/main.js
+++ palindroma/main.js
@@ -2,20 +2,23 @@
 // Chiedere all'utente di inserire una parola
 let parola = prompt("inserisci una parola");
 console.log(parola);
-const palindromeworld = "";
-for (let i = 0; i < parola.length; i--); {
-    let parolone = parola[i];
-    const palindromeworld = palindromeworld + parolone;
-    function palindroma(palindromeworld) {
-        // Creare una funzione per capire se la parola inserita è palindroma
+let palindromeworld = "";
 
-        if (parola == palindromeworld) {
-            alert("la parola é palindroma");
+for (let i = 0; i < parola.length; i++); {
 
-        } else {
-            alert("la parola non é palindroma");
+    palindromeworld = palindromeworld + parola;
 
-        }
+
+}
+palindroma(palindromeworld);
+function palindroma(palindromeworld) {
+    // Creare una funzione per capire se la parola inserita è palindroma
+
+    if (parola == palindromeworld) {
+        alert("la parola é palindroma");
+
+    } else {
+        alert("la parola non é palindroma");
 
     }
 
