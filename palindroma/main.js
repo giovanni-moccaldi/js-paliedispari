36d2ce75b73dbe1f6d549651331f8443af84e209
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-01-31 14:37:42 +0100

    notworking

---
+++ palindroma/main.js
@@ -0,0 +1,22 @@
+// Palidroma
+// Chiedere all'utente di inserire una parola
+let parola = prompt("inserisci una parola");
+console.log(parola);
+const palindromeworld = "";
+for (let i = 0; i < parola.length; i--); {
+    let parolone = parola[i];
+    const palindromeworld = palindromeworld + parolone;
+    function palindroma(palindromeworld) {
+        // Creare una funzione per capire se la parola inserita è palindroma
+
+        if (parola == palindromeworld) {
+            alert("la parola é palindroma");
+
+        } else {
+            alert("la parola non é palindroma");
+
+        }
+
+    }
+
+}
